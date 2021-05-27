3667a17f5a2543c859ae44bb3362c1f800b8a612
Evseev-A-Program <euseew2013@yandex.ru>
2021-05-28 01:50:01 +0400

    Саня чото добавил

--- src/main/java/com/example/dao/GamesDao.java
+++ src/main/java/com/example/dao/GamesDao.java
@@ -1,10 +1,20 @@
 package com.example.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 public class GamesDao {
 
 
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public GamesDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
 
+    public int[][] field = new int[10][10];
 
 
 
