652d83e29a716769df04b488e93a23479b234245
Evseev-A-Program <euseew2013@yandex.ru>
2021-06-04 03:22:17 +0400

    Саня чото добавил в аджакс

--- src/main/java/com/example/controllers/ajax/AjaxGameController.java
+++ src/main/java/com/example/controllers/ajax/AjaxGameController.java
@@ -2,9 +2,7 @@ package com.example.controllers.ajax;
 
 import com.example.dao.GamesDao;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AjaxGameController {
@@ -12,8 +10,14 @@ public class AjaxGameController {
     @Autowired
     private GamesDao gamesDao;
 
-    @GetMapping("/check_field")
-    public int Check(@RequestBody String str) {
+    @GetMapping("/check_field/{str}")
+    public int Game_Check(@PathVariable("str") String str) {
         return gamesDao.Check(str);
     }
+
+    @GetMapping("/add")
+    public int[][] add_ships() {
+        return gamesDao.add_ships();
+    }
+
 }
